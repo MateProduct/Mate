b48d99b71351f668c1a334a79b75730cb06acfa8
RUIRUIashui <1650227792@qq.com>
2022-11-11 17:11:28 -0500

    completed rspec for add course

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 describe CoursesController do
   describe 'POST /add_course/:uni  #add_course' do
-    # let!(:course) {FactoryBot.create(:course)}
+    let!(:course1) {FactoryBot.create(:course)}
     context 'add a course successfully' do
       it 'should save the course into database'do
-        expect {post :add_course, course: FactoryBot.attributes_for(:course)
+        expect {post :add_course, uni:course1.uni, course: FactoryBot.attributes_for(:course, course_no: "COMSE6156", section_no: "001")
         }.to change { Course.count }.by(1)
       end
     end
 
-    # context 'add a course successfully' do
-    #   it 'should save the course into database'do
-    #     expect {post :add_course, course: FactoryBot.attributes_for(:course)
-    #     }.to change { Course.count }.by(0)
-    #   end
-    # end
-    #
+    context 'add a course with a course_no that already exists' do
+      it 'should fail to save the course' do
+        post :add_course, uni:course1.uni, course: FactoryBot.attributes_for(:course, course_no: "COMSE6156", section_no: "001")
+        expect {post :add_course, uni:course1.uni, course: FactoryBot.attributes_for(:course, course_no: "COMSE6156", section_no: "001")
+        }.to change { Course.count }.by(0)
+      end
+    end
   end
 
 
--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :course do
     course_no { "COMSW4152" }
     uni { "aa1000" }
-    section_no { "001" }
+    section_no { "007" }
   end
 end
