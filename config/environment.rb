91974abd0f9b21d530152432fffc853a0840a1a5
sl4684 <sl4684@columbia.edu>
2022-12-17 00:15:00 -0500

    update domain to heroku domain

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rottenpotatoes::Application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
   :password => 'PUT IN PASSWORD', # This is the secret sendgrid API key which was issued during API key creation
-  :domain => 'yourdomain.com',
+  :domain => 'w4152-mate.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
