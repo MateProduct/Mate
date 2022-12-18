03294ea1640809e55eaf8f6e023cc0faf27245cf
Jiawen Chu <sab1025@bu.edu>
2022-12-17 19:46:57 -0500

    Edit host url in config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rottenpotatoes::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'w4152-mate.herokuapp.com' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
